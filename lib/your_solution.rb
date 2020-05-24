8eabd611d330490d54126185325ce017a6186bd0
Jack Ma <mapeiyao@gmail.com>
2020-05-24 14:13:26 +0000

    Automatically backed up by Learn

--- lib/your_solution.rb
+++ lib/your_solution.rb
@@ -1,6 +1,6 @@
 # Write your solution here
 
-lab_statement = "/"Hello World!/"""
+lab_statement = "Hello World!"
 puts lab_statement
 
 # This file should output "Hello World!" using a puts statement
